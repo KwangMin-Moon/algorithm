c2cf30499efec4253960a22758b3d4df10b6ba3d
Jason <timeli@naver.com>
2022-07-05 20:14:52 +0900

    문자열 알고리즘

--- section1/section3/4.js
+++ section1/section3/4.js
@@ -1,8 +1,32 @@
 function solution(s, t) {
-  const answer = s.split(t);
-  let result = [];
+  const answer = s;
+  let startNum = 1;
+  let result1 = [];
+  let result2 = [];
+  for (let i = 0; i < answer.length; i++) {
+    if (answer[i] !== t) {
+      startNum++;
+      result1.push(startNum);
+    } else {
+      startNum = 0;
+      result1.push(startNum);
+    }
+  }
+  for (let i = answer.length - 1; i >= 0; i--) {
+    if (answer[i] !== t) {
+      startNum++;
+      result2.push(startNum);
+    } else {
+      startNum = 0;
+      result2.push(startNum);
+    }
+  }
 
-  return answer;
+  let reverseResult2 = result2.reverse();
+  let result = result1.map((v, i) =>
+    v > reverseResult2[i] ? reverseResult2[i] : v
+  );
+  return result;
 }
 
 let str = 'teachermode';
--- section1/section3/5.js
+++ section1/section3/5.js
@@ -1,4 +1,21 @@
-function solution(s) {}
+function solution(s) {
+  let answer = [];
+  let p = 1;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== s[i + 1]) {
+      answer.push(s[i]);
+      if (p !== 1) {
+        answer.push(p);
+        p = 1;
+      }
+    }
+    if (s[i] === s[i + 1]) {
+      p += 1;
+    }
+  }
+
+  return answer.join('');
+}
 
 let str = 'KKHSSSSSSSE';
 console.log(solution(str));
