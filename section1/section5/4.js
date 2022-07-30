0abedafff2703853c2ed774ca5992f07943a5441
Jason <timeli@naver.com>
2022-07-30 19:23:17 +0900

    슬라이딩 윈도우

--- section1/section5/4.js
+++ section1/section5/4.js
@@ -1,5 +1,17 @@
 function solution(m, arr) {
-  let answer;
+  let answer = 0;
+  let rt = (lt = 0);
+  let sum = arr[lt];
+  while (rt < arr.length) {
+    if (sum <= m) {
+      answer += rt - lt + 1;
+      rt++;
+      sum += arr[rt];
+    } else if (sum > m) {
+      sum -= arr[lt];
+      lt++;
+    }
+  }
 
   return answer;
 }
--- section1/section5/5.js
+++ section1/section5/5.js
@@ -1,5 +1,15 @@
 function solution(k, arr) {
-  let answer;
+  let answer = 0;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < k; j++) {
+      sum += arr[i + j];
+    }
+    if (sum > answer) {
+      answer = sum;
+    }
+    sum = 0;
+  }
   return answer;
 }
 
